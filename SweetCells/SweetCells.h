12555fe9e4b913ff9743dd04e45fb498b5e98390
Null Zero <null-z@mail.ru>
2019-08-01 01:37:33 +0300

    Initial Commit

---
+++ SweetCells/SweetCells.h
@@ -0,0 +1,19 @@
+//
+//  SweetCells.h
+//  SweetCells
+//
+//  Created by Admin on 01/08/2019.
+//  Copyright © 2019 NZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SweetCells.
+FOUNDATION_EXPORT double SweetCellsVersionNumber;
+
+//! Project version string for SweetCells.
+FOUNDATION_EXPORT const unsigned char SweetCellsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SweetCells/PublicHeader.h>
+
+
